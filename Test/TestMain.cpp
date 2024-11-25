9e202fc27ee2ac236182effd1f22b92baf9f964d
Christopher <102330198+RiperPro03@users.noreply.github.com>
2024-11-25 09:37:50 +0100

    test: configuration des tests unitaires

---
+++ Test/TestMain.cpp
@@ -0,0 +1,9 @@
+//
+// Created by asrip on 21/11/2024.
+//
+#include "UnitTest.h" // Inclure votre bibliothèque personnalisée
+
+int main() {
+    RUN_ALL_TESTS(); // Exécute tous les cas de test enregistrés.
+    return 0;
+}
